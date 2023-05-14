fb774b48184c03f415567f53772aa5585fea661d
Amber Hasan <amberhasan@Ambers-MacBook-Pro-2.local>
2023-05-14 09:53:50 -0500

    Basic login screen that leads to home screen. From home screen, you can go to three other screens. All via buttons

--- App.js
+++ App.js
@@ -1,29 +1,122 @@
 // In App.js in a new project
 
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function LoginScreen(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Handle login logic here
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Login Screen</Text>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+        style={{
+          marginBottom: 10,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "gray",
+          width: 200,
+        }}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={{
+          marginBottom: 10,
+          padding: 10,
+          borderWidth: 1,
+          borderColor: "gray",
+          width: 200,
+        }}
+      />
+      <Button
+        title="Login"
+        onPress={() => {
+          props.navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
+}
 
 function HomeScreen(props) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Home Screen</Text>
-      <Button title="Go to Feed" onPress={()=>{
-        props.navigation.navigate('Feed');
-      }}/>
+      <Button
+        title="Go to Feed"
+        onPress={() => {
+          props.navigation.navigate("Feed");
+        }}
+      />
+      <Button
+        title="Go to Blog"
+        onPress={() => {
+          props.navigation.navigate("Blog");
+        }}
+      />
+      <Button
+        title="Go to Extra"
+        onPress={() => {
+          props.navigation.navigate("Extra");
+        }}
+      />
     </View>
   );
 }
 
 function FeedScreen(props) {
-  console.log('props',props.navigation);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Feed Screen</Text>
-      <Button title="Go to back" onPress={()=>{
-        props.navigation.goBack();
-      }}/>
+      <Button
+        title="Go back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  );
+}
+
+function BlogScreen(props) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Blog Screen</Text>
+      <Button
+        title="Go back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
+    </View>
+  );
+}
+
+function ExtraScreen(props) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Extra Screen</Text>
+      <Button
+        title="Go back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
     </View>
   );
 }
@@ -34,8 +127,11 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Feed" component={FeedScreen}/>
+        <Stack.Screen name="Feed" component={FeedScreen} />
+        <Stack.Screen name="Blog" component={BlogScreen} />
+        <Stack.Screen name="Extra" component={ExtraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
